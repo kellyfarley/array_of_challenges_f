4d3572e7c1b2843a0f738e952d46a166b10a6cef
Kelly Farley <farleykc@gmail.com>
2016-02-02 20:27:28 -0600

    bonus completed

--- donut_dibs.rb
+++ donut_dibs.rb
@@ -1,5 +1,5 @@
-donut_box1 = ["Boston Creme", "Boston Creme", "Boston Creme", "Choc Long John", "Choc Long John", "Vanilla Long John", "Vanilla Long John", "Old Fashioned", "Old Fashioned", "Old Fashioned", "French thing", "French Thing"]
-donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double choc cake", "double choc cake", "glazed", "glazed", "glazed", "choc cake", "choc cake", "choc cake"]
+#donut_box1 = ["Boston Creme", "Boston Creme", "Boston Creme", "Choc Long John", "Choc Long John", "Vanilla Long John", "Vanilla Long John", "Old Fashioned", "Old Fashioned", "Old Fashioned", "French thing", "French Thing"]
+#donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double choc cake", "double choc cake", "glazed", "glazed", "glazed", "choc cake", "choc cake", "choc cake"]
 
 
 #There are two boxes of delicious donuts sitting in front of you. They're everything you dreamed of and more. 
@@ -9,14 +9,24 @@ donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double ch
 
 #Create a hash where donuts are assigned to specific classmates
 
+donuts = {"Kelly" => "Old Fashioned", "Jennifer" => "Choc Long John", "Lena" => "Boston Creme"}
 
 #Return the donut of a specific person
 
+puts donuts["Kelly"]
 
 #Find three hash methods and experiment with them here:
 
+puts donuts.length
 
+donuts.delete("Lena")
+print donuts
+
+donuts.has_key?("Jennifer")
 
 #BONUS: Iterate through the hash and print a sentence for each student/donut pair. Example: "James calls dibs on a chocolate cake donut!"
 
-
+donuts = {"Kelly" => "Old Fashioned", "Jennifer" => "Choc Long John", "Lena" => "Boston Creme"}
+donuts.each do |key,array|
+	puts key + "calls dibs on" + array
+	end
